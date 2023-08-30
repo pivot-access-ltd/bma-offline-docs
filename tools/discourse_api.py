c3da8206d173753adc82f2545b753c7fec5ec3d3
billwear <wowear@gmail.com>
2023-08-30 17:13:14 -0500

    added /tools to master:
     - disced: discourse editing workflow manager
    
     - discourse_api.py: implements discourse api class and methods for reuse
    
     - itrpub: publishes image testing results from results repo to specified doc topic
    
     - update-image-testing-status: shellscript that runs itrpub with correct topic number

---
+++ tools/discourse_api.py
@@ -0,0 +1,62 @@
+import requests
+import time
+
+class DiscourseAPI:
+    def __init__(self, base_url, config):
+        self.base_url = base_url
+        self.config = config
+        self.session = requests.Session()
+        self.session.headers.update({"Api-Key": config["api_key"]})
+
+    def call_api(self, endpoint):
+        while True:
+            response = self.session.get(self.base_url + endpoint)
+            if response.status_code == 429:  # handle rate-limited access
+                wait_seconds = response.json()["wait_seconds"]
+                time.sleep(wait_seconds)
+            else:
+                response.raise_for_status()
+                return response.json()
+
+    def get_post(self, post_id):
+        return self.call_api(f"/posts/{post_id}.json")
+
+    def get_title(self, post_json):
+        return post_json["fancy_title"]
+
+    def get_topic(self, topic_id):
+        return self.call_api(f"/t/{topic_id}.json")
+
+    def get_topic_last_edit_timestamp(self, topic_id):
+        topic = self.get_topic(topic_id)
+        last_edit_timestamp = topic.get("last_posted_at")
+        return last_edit_timestamp
+
+    def post_to_topic(self, topic_id, markdown_content):
+        topic_json = self.get_topic(topic_id)
+        post_id = topic_json["post_stream"]["posts"][0]["id"]
+        endpoint = "/posts/" + str(post_id) + ".json"
+        payload = {
+            "topic_id": topic_id,
+            "raw": markdown_content,
+        }
+        response = self.session.post(self.base_url + endpoint, json=payload)
+        response.raise_for_status()
+
+    def update_topic_content(self, topic_id, markdown_content):
+        # Fetch the existing topic data
+        topic_endpoint = f"/t/{topic_id}.json"
+        topic_data = self.call_api(topic_endpoint)
+
+        # Update the existing content
+        post_id = topic_data["post_stream"]["posts"][0]["id"]
+        post_endpoint = f"/posts/{post_id}.json"
+        post_data = self.call_api(post_endpoint)
+        post_data["raw"] = markdown_content
+
+        # Update the post with the modified content
+        update_response = self.session.put(
+            self.base_url + post_endpoint, json=post_data
+        )
+        update_response.raise_for_status()
+        return update_response.status_code
